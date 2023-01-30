7e15d51565f76078a51cfa6e340d7738c14a6ba6
Vishal behal <105105573+vishalbehl25@users.noreply.github.com>
2023-01-30 15:35:04 +0530

    Delete sr.js

--- sr.js
+++
@@ -1,9 +0,0 @@
-var menuicon = document.querySelector(".menuicon")
-var sidebar = document.querySelector(".sidebar")
-var container = document.querySelector(".container")
-
-menuicon.onclick = function () {
-    sidebar.classList.toggle("smallbar") 
-    container.classList.toggle("largecontainer")
-        
-}
